4cb57b405c19f7f51ec2fbbf879b6328b0c2b241
nishuya <prathamyadav1817@gmail.com>
2025-12-13 22:36:05 +0530

    somee

--- server/Middlewere/usermidlewere.js
+++ server/Middlewere/usermidlewere.js
@@ -4,36 +4,43 @@ const jwt=require("jsonwebtoken");
 const User =require('../user')
 require("dotenv").config();
 
-const UserVerify = async (req,res,next) =>{
-  try{
-    const {Token}=req.cookies;
-    if(!Token){
-        throw new Error("Invalid user Token not found")
+
+
+const UserVerify = async (req, res, next) => {
+  try {
+    // Read token from Authorization header
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: "Invalid user Token not found" });
     }
-     const payload=jwt.verify(Token,process.env.JWT_KEY)
-     const {_id}=payload;
 
-     if(!_id){
-        throw new Error("token not founnd")
-     }
+    const Token = authHeader.split(' ')[1];
 
-     const user=await User.findById(_id);
-     if(!user){
-        throw new Error("User not found")
-     }
+    const payload = jwt.verify(Token, process.env.JWT_KEY);
+    const { _id } = payload;
 
-     const isBlocked=await Redis.exists(`token:${Token}`);
-     if(isBlocked){
-        throw new Error("Token not valid");
-     }
+    if (!_id) {
+      return res.status(401).json({ message: "Token not valid" });
+    }
 
-     req.user=user
-     next();
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  }catch(err){
-   console.log(err)
-    res.send("Invalid error :" + err)
+    const isBlocked = await Redis.exists(`token:${Token}`);
+    if (isBlocked) {
+      return res.status(401).json({ message: "Token is blocked" });
+    }
+
+    req.user = user;
+    next();
+
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Invalid user Token" });
   }
-}
+};
+
 
 module.exports=UserVerify;
